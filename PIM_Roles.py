86c1964197c97aff4acb9c740cbc722e2f32453d
Linus <element.oxygen@gmail.com>
2024-09-24 16:03:17 +0200

    split functions into new file

--- PIM_Roles.py
+++ PIM_Roles.py
@@ -1,6 +1,6 @@
 import asyncio
-import json
-from msgraph.generated.models.group import Group
+
+
 from creds import (
     azure_tenant_id,
     azure_client_id,
@@ -8,66 +8,24 @@ from creds import (
     confluence_page_id,
     confluence_token,
     confluence_url,
+    confluence_entraid_page_name,
 )
 from functions.confluence import (
     confluence_update_page,
-    style_text,
-    get_childid,
-    convert_to_html_table,
-    get_tables,
 )
 from atlassian import Confluence
 
-
+from functions.functions import (
+    get_assignments,
+    build_user_array,
+    get_documented_mappings,
+    check_new_mappings,
+    check_removed_mappings,
+)
 from pprint import pprint
 
 from functions.msgraphapi import GraphAPI
 from functions.log_config import logger
-
-
-async def get_assignments(pim):
-    assignment_dict = {}
-    assignments = await pim.get_role_eligibility_schedules()
-
-    for assignment in assignments:
-        principal = assignment.principal
-        role = assignment.role_definition.display_name
-        principal_display_name = assignment.principal.display_name
-
-        if isinstance(assignment.principal, Group):
-            logger.debug(f"Group: {principal_display_name} is assigned to {role}")
-            group_id = principal.id
-            group_members = await pim.get_group_members(group_id)
-            if len(group_members) > 0:
-                for group_member in group_members:
-                    member_display_name = group_member.display_name
-                    if role not in assignment_dict:
-                        assignment_dict[role] = [member_display_name]
-                    else:
-                        assignment_dict[role].append(member_display_name)
-        else:
-            if role not in assignment_dict:
-                assignment_dict[role] = [principal_display_name]
-            else:
-                assignment_dict[role].append(principal_display_name)
-    return assignment_dict
-
-
-def build_user_array(assignment_dict):
-
-    user_roles = {}
-    for role in assignment_dict:
-        for user in assignment_dict[role]:
-            if user not in user_roles:
-                user_roles[user] = [role]
-            else:
-                user_roles[user].append(role)
-
-    user_array = []
-    for user in user_roles:
-        for role in user_roles[user]:
-            user_array.append({"Benutzer": user, "Rolle": role})
-    return user_array
 
 
 async def main():
@@ -78,59 +36,26 @@ async def main():
     )
     confluence = Confluence(url=confluence_url, token=confluence_token)
 
+    # Get all Current Role Assignments from Azure PIM
     assignment_dict = await get_assignments(graph_client)
+    # Convert the results to a usable table
     user_array = build_user_array(assignment_dict)
-
-    title = "PIM: EntraID Rollen"
-
-    export_page_id = get_childid(confluence, confluence_page_id, title)
-    if export_page_id:
-        tables = get_tables(confluence, export_page_id)
-        existing_role_mappings = tables["tables_content"][0]
-
-        # Extract headers
-        headers = existing_role_mappings[0]
-        # Convert rows to dictionaries
-        role_mappings = [dict(zip(headers, row)) for row in existing_role_mappings[1:]]
-    else:
-        role_mappings = []
-        headers = []
-    for exported_role in user_array:
-        mapped = False
-        for existing_mapping in role_mappings:
-            if (
-                exported_role["Benutzer"] == existing_mapping["Benutzer"]
-                and exported_role["Rolle"] == existing_mapping["Rolle"]
-            ):
-                mapped = True
-                break
-        if not mapped:
-            for header in headers:
-                if header not in exported_role:
-                    exported_role[header] = ""
-            print(f"New mapping: {exported_role}")
-            role_mappings.append(exported_role)
-
-    # Sort by Username
-    role_mappings = sorted(role_mappings, key=lambda x: x["Benutzer"], reverse=False)
-
+    # Get Currently Documented Role Mappings
+    role_mappings, headers = get_documented_mappings(
+        confluence, confluence_page_id, confluence_entraid_page_name
+    )
+    # Check for new mappings
+    role_mappings = check_new_mappings(user_array, role_mappings, headers)
     # Remove mappings that are not in the export
-    for existing_mapping in role_mappings:
-        mapped = False
-        for exported_role in user_array:
-            if (
-                exported_role["Benutzer"] == existing_mapping["Benutzer"]
-                and exported_role["Rolle"] == existing_mapping["Rolle"]
-            ):
-                mapped = True
-                break
-        if not mapped:
-            print(f"Mapping not found: {existing_mapping}")
-            role_mappings.remove(existing_mapping)
+    role_mappings = check_removed_mappings(user_array, role_mappings)
 
+    # Sort the mappings by user name
+    role_mappings = sorted(
+        role_mappings, key=lambda x: (x["Benutzer"], x["Rolle"]), reverse=False
+    )
     confluence_update_page(
         confluence=confluence,
-        title=title,
+        title=confluence_entraid_page_name,
         parent_id=confluence_page_id,
         table=role_mappings,
         representation="storage",
---
+++ functions/functions.py
@@ -0,0 +1,127 @@
+import json
+from functions.log_config import logger
+from msgraph.generated.models.group import Group
+from functions.confluence import (
+    confluence_update_page,
+    style_text,
+    get_childid,
+    convert_to_html_table,
+    get_tables,
+)
+
+async def get_assignments(pim):
+    assignment_dict = {}
+    assignments = await pim.get_role_eligibility_schedules()
+
+    for assignment in assignments:
+        principal = assignment.principal
+        role = assignment.role_definition.display_name
+        principal_display_name = assignment.principal.display_name
+
+        if isinstance(assignment.principal, Group):
+            logger.debug(f"Group: {principal_display_name} is assigned to {role}")
+            group_id = principal.id
+            group_members = await pim.get_group_members(group_id)
+            if len(group_members) > 0:
+                for group_member in group_members:
+                    member_display_name = group_member.display_name
+                    if role not in assignment_dict:
+                        assignment_dict[role] = [member_display_name]
+                    else:
+                        assignment_dict[role].append(member_display_name)
+        else:
+            if role not in assignment_dict:
+                assignment_dict[role] = [principal_display_name]
+            else:
+                assignment_dict[role].append(principal_display_name)
+    return assignment_dict
+
+
+def build_user_array(assignment_dict):
+
+    user_roles = {}
+    for role in assignment_dict:
+        if privileged_role_filter(role):
+            for user in assignment_dict[role]:
+                if user not in user_roles:
+                    user_roles[user] = [role]
+                else:
+                    user_roles[user].append(role)
+
+    user_array = []
+    for user in user_roles:
+        for role in user_roles[user]:
+            user_array.append({"Benutzer": user, "Rolle": role})
+    return user_array
+
+
+def get_documented_mappings(confluence, confluence_page_id, sub_page_name):
+    export_page_id = get_childid(confluence, confluence_page_id, sub_page_name)
+    if export_page_id:
+        tables = get_tables(confluence, export_page_id)
+        existing_role_mappings = tables["tables_content"][0]
+
+        # Extract headers
+        headers = existing_role_mappings[0]
+        # Convert rows to dictionaries
+        role_mappings = [dict(zip(headers, row)) for row in existing_role_mappings[1:]]
+    else:
+        role_mappings = []
+        headers = []
+    return role_mappings, headers
+
+
+def check_new_mappings(user_array, role_mappings, headers):
+    for exported_role in user_array:
+        mapped = False
+        for existing_mapping in role_mappings:
+            if (
+                exported_role["Benutzer"] == existing_mapping["Benutzer"]
+                and exported_role["Rolle"] == existing_mapping["Rolle"]
+            ):
+                mapped = True
+                break
+        if not mapped:
+            for header in headers:
+                if header not in exported_role:
+                    exported_role[header] = ""
+            print(f"New mapping: {exported_role}")
+            role_mappings.append(exported_role)
+    return role_mappings
+
+
+def check_removed_mappings(user_array, role_mappings):
+    for existing_mapping in role_mappings:
+        mapped = False
+        for exported_role in user_array:
+            if (
+                exported_role["Benutzer"] == existing_mapping["Benutzer"]
+                and exported_role["Rolle"] == existing_mapping["Rolle"]
+            ):
+                mapped = True
+                break
+        if not mapped:
+            print(f"Mapping not found: {existing_mapping}")
+            role_mappings.remove(existing_mapping)
+    return role_mappings
+
+
+def privileged_role_filter(role):
+    privileged_groups = [
+        "Application Administrator",
+        "Authentication Administrator",
+        "Cloud Device Administrator",
+        "Conditional Access Administrator",
+        "Global Administrator",
+        "Global Reader",
+        "Intune Administrator",
+        "Privileged Role Administrator",
+        "Security Administrator",
+        "Security Operator",
+        "Security Reader",
+        "User Administrator",
+    ]
+    if role in privileged_groups:
+        return True
+    else:
+        return False
